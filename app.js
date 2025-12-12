806be1b5ae9a54196cdca903592ddff1c7e321a2
Black Crow Creative Lab <tracy@blackcrowcreativelab.com>
2025-12-12 13:32:18 -0800

    building out additional pages, set up header, footer ejs

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 import express from "express";
-const app = express();
-const port = 3000;
 
+const app = express();
+const port = 3001;
+app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
 
+
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("index");
 });
+
+app.get("/projects", (req, res) => {
+    res.render("projects");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const currentPage = window.location.pathname.split("/").pop(); 
-    const navLinks = document.querySelectorAll(".nav-link");
 
-    navLinks.forEach(link => {
-        const linkPage = link.getAttribute("href");
-        if (linkPage === currentPage) {
-            link.classList.add("active");
-            link.setAttribute("aria-current", "page");
-        } else {
-            link.classList.remove("active");
-            link.removeAttribute("aria-current");
-        }
-    });
-});
+ 
