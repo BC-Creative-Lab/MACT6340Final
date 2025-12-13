fd6eecdcaa770bc54f47b2b81feaed287f7fe980
Black Crow Creative Lab <tracy@blackcrowcreativelab.com>
2025-12-13 13:43:09 -0800

    about page done, error page added, images added

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+// if no routes matched, show 404 page
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+// general error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).render("error.ejs", {
+    title: "Server Error (500)",
+    body: "Something went wrong on our end.",
+  });
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
